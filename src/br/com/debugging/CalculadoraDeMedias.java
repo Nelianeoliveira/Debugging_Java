f03c8a4b5887eb9c2d24707fd1d5e5cf97bff6b4
Neliane de Oliveira <92684306+Nelianeoliveira@users.noreply.github.com>
2022-07-25 15:57:30 -0300

    Projeto

---
+++ src/br/com/debugging/CalculadoraDeMedias.java
@@ -0,0 +1,26 @@
+package br.com.debugging;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class CalculadoraDeMedias {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] alunos = {"Neliane", "Lucas", "Bruna", "David"};
+
+        double media = calculaMediaDaTurma(alunos, scan);
+
+        System.out.printf("Média da turma %.1f" , media);
+    }
+
+    public static double calculaMediaDaTurma(String [] alunos, Scanner scanner) {
+
+        double soma = 0;
+        for (String aluno : alunos) {
+          System.out.printf("Nota do aluno %s: ", aluno);
+          double nota = scanner.nextDouble();
+          soma += nota;
+        }
+         return soma / alunos.length;
+    }
+}
